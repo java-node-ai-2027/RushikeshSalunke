dd59617ed037b749f73084d6e86e5d19598171ba
rushikesh.salunke@thinkitive.com <rushikesh.salunke@thinkitive.com>
2026-09-02 12:09:43 +0530

    2nd september Update

---
+++ src/script.js
@@ -0,0 +1,66 @@
+// var ans = new Promise((resolve,reject)=>{
+//     var n = Math.floor(Math.random()*10);
+
+//     if(n<5){
+//         return resolve() ;
+//     }else{
+//         return reject();
+//     }
+// })
+
+// ans.then(function(){
+//     console.log("below ");
+// })
+// .catch(function(){
+//     console.log("above");
+// })
+
+//     var m = Math.floor(Math.random() * 100);
+//     console.log(m);
+    
+//// var figlett = require('figlet');
+//import figlet from "figlet";
+// async function doStuff() {
+//   const text = await figlet.text("!!");
+//   console.log(text);
+// }
+// doStuff();
+
+// async function dosome(){
+//     let text = await figlet.text("Rushikesh");
+//     console.log(text); 
+// }
+// dosome();
+
+// import express from "express";
+// let app = express();
+// app.get("/",(req,res)=>{
+//     res.send("congrats server is ready priti");
+// })
+// app.get("/contact",(req,res)=>{
+//     res.send("congrats server is ready in contacts section ");
+// })
+// app.get("/profile",(req,res)=>{
+//     res.send("congrats server is ready  and getting profile info ");
+// })
+
+// app.listen(3000,()=>{
+//     console.log("server is running on a local host 30000");    
+// })
+
+let start = "mahi";
+console.log(start);
+
+setTimeout(()=>{
+   console.log("waiting for 3 second for shruti ");
+},3000)
+
+let end = "priti";
+console.log(end);
+
+
+
+
+
+
+
